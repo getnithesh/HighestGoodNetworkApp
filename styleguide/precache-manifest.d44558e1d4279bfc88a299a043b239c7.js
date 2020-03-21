b5fa545c8b2bc2062ff95a6e561891b084832a01
Simon X <simonxiong11@gmail.com>
2020-03-21 12:28:10 -0700

    track Styleguide build

---
+++ styleguide/precache-manifest.d44558e1d4279bfc88a299a043b239c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ace0a8f4e474d6a71b4a",
+    "url": "build/2.ace0a8f4.js"
+  },
+  {
+    "revision": "79d32352677aac1e84b3af723c7acc7d",
+    "url": "build/2.ace0a8f4.js.LICENSE.txt"
+  },
+  {
+    "revision": "47e562ce73a2b423b95d",
+    "url": "build/bundle.47e562ce.js"
+  },
+  {
+    "revision": "62a8c16a276edfec9bdf",
+    "url": "build/main.62a8c16a.js"
+  },
+  {
+    "revision": "c75f52e1ea9db6489752e3614a381602",
+    "url": "index.html"
+  },
+  {
+    "revision": "ace0a8f4e474d6a71b4a",
+    "url": "static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "62a8c16a276edfec9bdf",
+    "url": "static/css/main.9e16672f.chunk.css"
+  }
+]);
